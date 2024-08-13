41ead3199a26e3d3b3f3aa11a78b34f748f384a7
Andy Pak <im.andy.pak@gmail.com>
2024-08-13 19:03:16 -0400

    Create gemfile and add dependencies
    Autocorrected offenses with rubocop in main.rb

--- caesar-cipher/main.rb
+++ caesar-cipher/main.rb
@@ -1,7 +1,7 @@
 def caesar_cipher(word, key)
-  alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
-  alphabet_up = alphabet.join("").upcase.split("")
-  word.split("").map! do |char|
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  alphabet_up = alphabet.join('').upcase.split('')
+  word.split('').map! do |char|
     if alphabet.include?(char)
       char.ord + key > 122 ? char.ord + key - 26 : char.ord + key
     elsif alphabet_up.include?(char)
@@ -10,5 +10,5 @@ def caesar_cipher(word, key)
       char
     end
   end
-  .map! {|num| num.chr}.join("")
+  .map! { |num| num.chr }.join('')
 end
